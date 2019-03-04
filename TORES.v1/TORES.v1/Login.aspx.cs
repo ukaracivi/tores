728b179989f8aa912fb0ccbabc0fa10eece6e380
Ümit <umit.karacivi@outlook.com>
2019-03-04 14:23:30 +0300

    Açılkama

---
+++ TORES.v1/TORES.v1/Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TORES.v1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btonOK_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("Default.aspx");
+        }
+    }
+}
